55e4ac061f4362d4b8e5838eeb0a2691b182eb6a
divaprates <divasilvaprates@gmail.com>
2023-10-18 20:34:12 -0300

    feat: repassando os parametros de componentes pais para os filhos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+
+  @Input() gameCover: string = '';
+  @Input() gameLabel: string = '';
+  @Input() gameType: string = '';
+  @Input() gamePrice: string = '';
 
 }
---
+++ src/app/model/Game.ts
@@ -0,0 +1,6 @@
+export interface Game {
+    gameCover: string;
+    gameLabel: string;
+    gameType: string;
+    gamePrice: string;
+}
