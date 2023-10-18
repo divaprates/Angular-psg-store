f3c95a6d80c4411a2aba3a86d16d45a33600c987
divaprates <divasilvaprates@gmail.com>
2023-10-18 19:53:57 -0300

    feat: criando o label

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-label.component.scss']
 })
 export class CardLabelComponent {
+
+  @Input()
+  gameLabel: string = '';
 
 }
