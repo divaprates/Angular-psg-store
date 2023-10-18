32e0c20ba5968de1f310dd5311046e1603f5a984
divaprates <divasilvaprates@gmail.com>
2023-10-18 20:44:36 -0300

    feat: refatoração

---
+++ src/app/dataFake/GameFake.ts
@@ -0,0 +1,10 @@
+import { Game } from "../model/Game";
+
+export const gameFake: Game[] = [
+    {
+        gameCover: "assets/bt-5.jpg",
+        gameLabel: "DIGITAL",
+        gameType: "Digital PS4",
+        gamePrice: "R$ 255,99",
+    }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gameFake } from 'src/app/dataFake/GameFake';
+import { Game } from 'src/app/model/Game';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  
+  gameList: Game[] = [];
+  
+  ngOnInit(): void {
+   this.gameList = gameFake;
+  }
+  
 }
