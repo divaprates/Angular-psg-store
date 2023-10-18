6c077eee09433418a417d33c7a88bfac468b8fd5
divaprates <divasilvaprates@gmail.com>
2023-10-18 20:17:44 -0300

    feat: Criando o card-pricing

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-pricing.component.scss']
 })
 export class CardPricingComponent {
+
+  @Input() gameType: string = '';
+  @Input() gamePrice: string = '';
+  
 
 }
